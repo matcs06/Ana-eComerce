a6a5b61ea9e6314aa3e5190dce24590ba09e2501
Mateus Costa <matcs06@gmail.com>
2020-11-11 00:57:43 -0300

    Adding migrations

---
+++ src/shared/infra/typeorm/migrations/1595143888714-CreateOrder.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrder1595143888714 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customer_name',
+            type: 'varchar',
+          },
+          {
+            name: 'customer_address',
+            type: 'varchar',
+          },
+          {
+            name: 'payment_method',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
