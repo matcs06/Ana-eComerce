d50eb724d8139c484485ba1cb1fce19bf8fa2b8c
Mateus Costa <matcs06@gmail.com>
2020-11-10 23:16:32 -0300

    Removing dist

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
       "@shared": "./src/shared"
      }
    }],
+   "transform-es2015-modules-commonjs",
    "babel-plugin-transform-typescript-metadata",
    ["@babel/plugin-proposal-decorators", {"legacy":true}],
    ["@babel/plugin-proposal-class-properties", {"loose":true}]
